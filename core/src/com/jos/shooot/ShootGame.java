36ba04374471842311e7685aad9b16868b6d1604
josmas <josmasflores@gmail.com>
2015-06-06 17:52:56 +0100

    reformat and add space bar handler

--- core/src/com/jos/shooot/ShootGame.java
+++ core/src/com/jos/shooot/ShootGame.java
@@ -10,58 +10,62 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ShootGame extends ApplicationAdapter {
-	private Texture dude;
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Sprite dudeSprite;
+  private Texture dude;
+  private OrthographicCamera camera;
+  private SpriteBatch batch;
+  private Sprite dudeSprite;
 
-	@Override
-	public void create () {
+  @Override
+  public void create() {
 
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 600, 480);
+    camera = new OrthographicCamera();
+    camera.setToOrtho(false, 600, 480);
 
-		dude = new Texture("dude_gun_sprite.png");
-		dudeSprite = new Sprite(dude, 0, 0, 40, 48);
-		batch = new SpriteBatch();
+    dude = new Texture("dude_gun_sprite.png");
+    dudeSprite = new Sprite(dude, 0, 0, 40, 48);
+    batch = new SpriteBatch();
 
-	}
+  }
 
-	float dudeSpeed = 10.0f;
-	float dudeX;
-	float dudeY;
-	boolean flip = false;
+  float dudeSpeed = 10.0f;
+  float dudeX;
+  float dudeY;
+  boolean flip = false;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+  @Override
+  public void render() {
+    Gdx.gl.glClearColor(1, 1, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)){
-			dudeX -= Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
-			if (flip) {
-				dudeSprite.flip(flip, false);
-				flip = false;
-			}
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
-			dudeX += Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
-			if (!flip){
-				dudeSprite.flip(true, false);
-				flip = true;
-			}
-		}
+    if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+      dudeX -= Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
+      if (flip) {
+        dudeSprite.flip(flip, false);
+        flip = false;
+      }
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+      dudeX += Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
+      if (!flip) {
+        dudeSprite.flip(true, false);
+        flip = true;
+      }
+    }
 
-		batch.begin();
-		batch.draw(dudeSprite, dudeX, dudeY);
-		batch.end();
+    if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+      dudeY += Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
+    }
 
-	}
+    batch.begin();
+    batch.draw(dudeSprite, dudeX, dudeY);
+    batch.end();
 
-	@Override
-	public void dispose() {
-		//TODO (jos) What else do we need to dispose of?
-		batch.dispose();
-		dude.dispose();
-	}
+  }
+
+  @Override
+  public void dispose() {
+    //TODO (jos) What else do we need to dispose of?
+    batch.dispose();
+    dude.dispose();
+  }
 }
