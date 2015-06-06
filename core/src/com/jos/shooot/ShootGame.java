ef1d8b052bcc071788185cfd358176830e1bc943
josmas <josmasflores@gmail.com>
2015-06-06 17:27:30 +0100

    adds a dude sprite with movement on x axis

--- core/src/com/jos/shooot/ShootGame.java
+++ core/src/com/jos/shooot/ShootGame.java
@@ -2,26 +2,66 @@ package com.jos.shooot;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ShootGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture dude;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Sprite dudeSprite;
+
 	@Override
 	public void create () {
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 600, 480);
+
+		dude = new Texture("dude_gun_sprite.png");
+		dudeSprite = new Sprite(dude, 0, 0, 40, 48);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 	}
+
+	float dudeSpeed = 10.0f;
+	float dudeX;
+	float dudeY;
+	boolean flip = false;
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)){
+			dudeX -= Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
+			if (flip) {
+				dudeSprite.flip(flip, false);
+				flip = false;
+			}
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
+			dudeX += Gdx.graphics.getDeltaTime() * dudeSpeed * 10;
+			if (!flip){
+				dudeSprite.flip(true, false);
+				flip = true;
+			}
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(dudeSprite, dudeX, dudeY);
 		batch.end();
+
+	}
+
+	@Override
+	public void dispose() {
+		//TODO (jos) What else do we need to dispose of?
+		batch.dispose();
+		dude.dispose();
 	}
 }
--- desktop/src/com/jos/shooot/desktop/DesktopLauncher.java
+++ desktop/src/com/jos/shooot/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jos.shooot.ShootGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ShootGame(), config);
-	}
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.title = "Shoot";
+    config.width = 600;
+    config.height = 480;
+    new LwjglApplication(new ShootGame(), config);
+  }
 }
